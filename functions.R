81f0285ab3e0c3afdda1f695776b929298823a65
Fabricio dos Anjos <fabriciodosanjos14@gmail.com>
2024-07-02 19:20:09 -0300

    Add files

--- functions.R
+++ functions.R
@@ -42,3 +42,95 @@ cleaning_ncbi_database <- function(raw_database, min_sequence_length) {
   
   system(paste0("wsl tr -d '[]\"' < ", database_cleaned, " > temp_database_cleaned && mv temp_database_cleaned ", database_cleaned))
 }
+
+
+# Loading ...
+
+
+library(rgbif)
+library(sf)
+library(concaveman)
+library(ggplot2)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(leaflet)
+library(mapedit)
+
+# Configurar a busca no GBIF
+taxa <- "Pristis pristis"
+continent <- c('north_america')
+fields <- c("name", "species", "genus", "family", "decimalLongitude", "decimalLatitude", "basisOfRecord")
+limit <- 400 # Número de registros por solicitação
+start <- 0
+all_data <- list()
+
+observation_type <- "PRESERVED_SPECIMEN" # Filtro para observações de museu
+
+# Buscar dados no GBIF
+repeat {
+  result <- occ_search(continent = continent, hasCoordinate = TRUE, scientificName = taxa, basisOfRecord = observation_type, fields = fields, limit = limit, start = start)
+  if (nrow(result$data) == 0) break
+  all_data <- append(all_data, list(result$data))
+  start <- start + limit
+}
+
+# Combinar todos os dados em um único data frame
+data <- do.call(rbind, all_data)
+
+# Excluir linhas com coordenadas ausentes e onde a coluna "species" tem valores NA
+data <- data[!is.na(data$decimalLongitude) & !is.na(data$decimalLatitude) & !is.na(data$species), ]
+
+# Converter dados para sf
+sf_data <- st_as_sf(data, coords = c("decimalLongitude", "decimalLatitude"), crs = 4326)
+polygon <- concaveman(sf_data)
+
+# Obter o mapa da América do Sul
+south_america <- ne_countries(scale = "medium", continent = "South America", returnclass = "sf")
+
+# Filtrar pontos dentro e fora da área terrestre da América do Sul
+within_sa <- st_within(sf_data, south_america, sparse = FALSE)
+within_sa_indices <- apply(within_sa, 1, any)
+sf_data_within_sa <- sf_data[within_sa_indices, ]
+sf_data_outside_sa <- sf_data[!within_sa_indices, ]
+
+# Ensure the species column is a character vector
+sf_data_within_sa$species <- as.character(sf_data_within_sa$species)
+
+# Criar mapas interativos para seleção manual de pontos
+map_within_sa <- leaflet(sf_data_within_sa) %>%
+  addProviderTiles(providers$Esri) %>%
+  addCircleMarkers(radius = 2, color = "blue", label = ~species)
+
+map_outside_sa <- leaflet(sf_data_outside_sa) %>%
+  addProviderTiles(providers$Esri) %>%
+  addCircleMarkers(radius = 2, color = "red", label = ~species)
+
+# Exibir mapas interativos
+map_within_sa
+map_outside_sa
+
+# Selecionar manualmente pontos a serem excluídos
+selected_within_sa <- editMap(map_within_sa, title = "Selecione os pontos a serem excluídos dentro da área terrestre")
+selected_outside_sa <- editMap(map_outside_sa, title = "Selecione os pontos a serem excluídos fora da área terrestre")
+
+# Converter os pontos selecionados em um objeto sf para manipulação
+selected_within_sa_sf <- st_as_sf(selected_within_sa$finished)
+selected_outside_sa_sf <- st_as_sf(selected_outside_sa$finished)
+
+# Excluir pontos selecionados
+sf_data_within_sa <- sf_data_within_sa[!st_intersects(sf_data_within_sa, selected_within_sa_sf, sparse = FALSE), ]
+sf_data_outside_sa <- sf_data_outside_sa[!st_intersects(sf_data_outside_sa, selected_outside_sa_sf, sparse = FALSE), ]
+
+# Criar mapas interativos para seleção manual de pontos
+map_within_sa_ <- leaflet(sf_data_within_sa) %>%
+  addProviderTiles(providers$Esri) %>%
+  addCircleMarkers(radius = 2, color = "blue", label = ~species)
+
+map_within_sa_
+
+map_outside_sa_ <- leaflet(sf_data_outside_sa) %>%
+  addProviderTiles(providers$Esri) %>%
+  addCircleMarkers(radius = 2, color = "red", label = ~species)
+
+
+
