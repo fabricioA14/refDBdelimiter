4becdc3ef18da432cf8feee93c69cc23a01e8c8b
fabricioA14 <fabriciodosanjos14l@gmail.com>
2025-08-31 17:08:59 -0300

    Updated Subset and Blast functions

--- R/refDB_Blast.R
+++ R/refDB_Blast.R
@@ -71,6 +71,7 @@
 #' )}
 #' }
 #' @import readr
+#' @import Biostrings readDNAStringSet
 #' @importFrom dplyr group_by top_n rename distinct filter mutate select ungroup case_when left_join
 #' @importFrom plyr match_df
 #' @importFrom readr read_table
@@ -78,6 +79,7 @@
 #' @importFrom stringr str_split
 #' @importFrom tidyr pivot_wider
 #' @importFrom utils read.table write.table
+#' @importFrom rentrez entrez_summary
 #' @importFrom taxizedb classification db_download_ncbi name2taxid src_ncbi
 #' @export
 refDB_Blast <- function(Directory, Database_File, otu_table = "otu_table.txt", query = "otus.fasta", task = "megablast", out = "blast.txt", 
@@ -162,15 +164,15 @@ refDB_Blast <- function(Directory, Database_File, otu_table = "otu_table.txt", q
                 " -num_threads ", num_threads, 
                 optional_params))
   
-  system(paste0("wsl blastn -query ", paste0(linux_path, query), 
-                " -task ", task, 
-                " -db ", paste0(linux_path, Database_File), 
-                " -out ", out, 
-                " -outfmt 6",  # Set output format to tabular
-                " -max_target_seqs 50", 
-                " -perc_identity 95", 
-                " -qcov_hsp_perc 95", 
-                " -num_threads 6"))
+  #system(paste0("wsl blastn -query ", paste0(linux_path, query), 
+  #              " -task ", task, 
+  #              " -db ", paste0(linux_path, Database_File), 
+  #              " -out ", out, 
+  #              " -outfmt 6",  # Set output format to tabular
+  #              " -max_target_seqs 50", 
+  #              " -perc_identity 95", 
+  #              " -qcov_hsp_perc 95", 
+  #              " -num_threads 6"))
   
   system(paste0("wsl tr -d '#' < ", otu_table, " > temp_raw_database && mv temp_raw_database ", otu_table))
   
@@ -178,197 +180,116 @@ refDB_Blast <- function(Directory, Database_File, otu_table = "otu_table.txt", q
   
   Samples <- read.table(paste0(Directory, otu_table), sep = "\t", header = TRUE)
   
-  colnames(Samples)[1] <- "qseqid"
+  fasta_seq <- Biostrings::readDNAStringSet(paste0(Directory, query))
   
   ########## Files = csv + ID taxa ##########
   colnames(csv1) <- c("qseqid", "seqid", "pident", "length", "mismatch", "gapopen", "qstart", "qend", "sstart", "send", "evalue", "bitscore")
   
+  library(dplyr)
+  
   # Separate only the max value of match per ZOTU
-  cl.max_ <- csv1 %>% group_by(qseqid) %>% top_n(1, pident)
+  cl.max_ <- csv1 %>% group_by(qseqid) %>% slice_max(pident, n = 1, with_ties = FALSE)
   
-  # Organizing the species names
-  cl.max_$seqid <- gsub("\\d+_", "", cl.max_$seqid)
-  cl.max_$seqid <- gsub("_", " ", cl.max_$seqid)
-  cl.max_$seqid <- sub("\\b(sp\\.?\\s+).*", "\\1", cl.max_$seqid)
+  library(rentrez)
+  library(taxizedb)
   
-  # Split each cell into "characters" separated by blank spaces
-  split_seqid <- strsplit(cl.max_$seqid, "\\s+")
-  
-  # Exclude characters after the second "character" of each cell
-  cl.max_$seqid <- sapply(split_seqid, function(chars) {
-    paste(chars[1:2], collapse = " ")
-  })
-  
-  # Separate only the min value of match per OTU - Evalue 
-  cl.max_ <- cl.max_ %>% group_by(qseqid) %>% top_n(-1, evalue)
-  
-  # Separate only the min value of match per OTU - Bitscore 
-  cl.max_ <- cl.max_ %>% group_by(qseqid) %>% top_n(-1, bitscore)
-  
-  cl.max_ <- cl.max_[!duplicated(cl.max_[1]), ]
-  
-  ids <- paste(cl.max_$qseqid, cl.max_$seqid, sep = " ")
-  
-  # data source: NCBI
+  # Validação do NCBI
   db_download_ncbi()
   src <- src_ncbi()
   
-  taxa_to_id <- name2taxid(sapply(strsplit(ids, " "), function(x) paste(x[2:3], collapse = " ")))
+  get_taxid <- function(accession) {
+  summary <- entrez_summary(db = "nuccore", id = accession)
+  return(summary$taxid)}
   
-  taxa_names <- classification(taxa_to_id, db = "ncbi")
+  accession_numbers <- cl.max_$seqid
+
+  # Aplicar a função a cada accession number
+  taxids <- sapply(accession_numbers, get_taxid)
   
-  # Function to check if an object is a DataFrame
+  #accession_numbers <- taxizedb::classification(accession_numbers, db = "ncbi")
+  
+  #taxids <- sapply(accession_numbers[1:5], get_taxid)
+  #taxids
+  taxids <- taxizedb::classification(taxids, db = "ncbi")
+  taxids_ <- taxids
+  
+  taxa_names <- taxids
+  
+  accession_id <- data.frame(Acession = accession_numbers, tax_ids = names(taxa_names))
+  
+  # To test if we have a dataframe after extract each ID
   is_dataframe <- function(obj) {
     inherits(obj, "data.frame")
   }
   
-  # Iterate through each sublist
+  #If a df is not generated, we create an empty one (to standardize the "main" output object)
   for (i in seq_along(taxa_names)) {
-    # If it's not a DataFrame, create a new DataFrame with NA values
     if (!is_dataframe(taxa_names[[i]])) {
       taxa_names[[i]] <- data.frame(name = NA, rank = NA, id = NA)
     }
   }
   
-  # Replace names based on the vector
-  names(taxa_names) <- ids
-  
+  # Here we unify all dfs. Each one has a ASV/OTU name associated (for further validations)
   taxa_names_df <- do.call(rbind, lapply(names(taxa_names), function(name) {
     df <- taxa_names[[name]]
-    df$Taxa <- name
+    df$taxid <- name
     return(df)
   }))
   
-  sum(grepl("phylum", taxa_names_df$rank, ignore.case = TRUE))
-  
-  taxa_names_df <- taxa_names_df[, c("Taxa", names(taxa_names_df)[1:(ncol(taxa_names_df) - 1)])] 
+  taxa_names_df <- taxa_names_df[, c("taxid", names(taxa_names_df)[1:(ncol(taxa_names_df) - 1)])]
   taxa_names_df <- taxa_names_df[, -ncol(taxa_names_df)]
   
   taxa_names_df <- taxa_names_df %>%
     filter(rank %in% c("kingdom", "phylum", "class", "order", "family", "genus", "species", NA)) %>%
-    group_by(Taxa) %>%
+    group_by(taxid) %>%
     distinct()
   
-  taxa_remaining <- taxa_names_df$Taxa[is.na(taxa_names_df$rank)]
+  identified_otus <- taxa_names_df %>%
+  tidyr::pivot_wider(names_from = rank, values_from = name)
+
+  csv1_merged <- cl.max_ %>%
+  left_join(accession_id, by = c("seqid" = "Acession"), relationship ="many-to-many") %>%
+  relocate(tax_ids, .after = 2) %>%  
+  distinct()           
   
-  # Check if taxa_remaining is not empty
-  if (length(taxa_remaining) > 0) {
-    # If taxa_remaining is not empty, run this block
-    taxa_with_na <- sapply(strsplit(taxa_remaining, " "), "[", 2)
-    taxa_with_na <- data.frame(name = taxa_with_na)
-    row.names(taxa_with_na) <- taxa_remaining
-    
-    # Add the ID column to taxa_with_na based on matching names
-    taxa_with_na_rank <- name2taxid(taxa_with_na$name, out_type = "summary")
-    taxa_with_na_with_id <- left_join(taxa_with_na, taxa_with_na_rank, by = "name")
-    row.names(taxa_with_na_with_id) <- taxa_remaining
-    
-    # Match the values in taxa with the values in df$name
-    remaining_taxa <- classification(taxa_with_na_with_id$id, db = "ncbi")
-    names(remaining_taxa) <- taxa_remaining
-    
-    # Iterate through each sublist
-    for (i in seq_along(remaining_taxa)) {
-      # If it's not a DataFrame, create a new DataFrame with NA values
-      if (!is_dataframe(remaining_taxa[[i]])) {
-        remaining_taxa[[i]] <- data.frame(name = NA, rank = NA, id = NA)
-      }
-    }
-    
-    taxa_names_df_rem <- do.call(rbind, lapply(names(remaining_taxa), function(name) {
-      df <- remaining_taxa[[name]]
-      df$Taxa <- name
-      return(df)
-    }))
-    
-    taxa_names_df_rem <- taxa_names_df_rem[, c("Taxa", names(taxa_names_df_rem)[1:(ncol(taxa_names_df_rem) - 1)])]
-    taxa_names_df_rem <- taxa_names_df_rem[, -ncol(taxa_names_df_rem)]
-    
-    taxa_names_df <- taxa_names_df %>%
-      filter(rank %in% c("kingdom", "phylum", "class", "order", "family", "genus", "species", NA)) %>%
-      group_by(Taxa) %>%
-      distinct()
-    
-    taxa_names_df <- taxa_names_df[complete.cases(taxa_names_df), ]
-    
-    # Combine the dataframes using rbind
-    all_taxa <- data.frame(rbind(taxa_names_df, taxa_names_df_rem))
-    
-    #library(tidyr)
-    #library(stringr)
-    
-    identified_otus <- all_taxa %>% 
-      pivot_wider(names_from = rank, values_from = name)
-    
-  } else {
-    # If taxa_remaining is empty, run this block
-    taxa_names_df <- taxa_names_df[complete.cases(taxa_names_df), ]
-    
-    identified_otus <- taxa_names_df %>%
-      pivot_wider(names_from = rank, values_from = name)
-  }
+  #write.table(csv1_merged, file = paste0("identified_blast.txt"), sep = "\t", row.names = F, col.names = T)
   
-  # Split the content based on the first blank space
-  split_content <- str_split(identified_otus$Taxa, " ", n = 2)
-  
-  # Extract the first part (characters before the first blank space)
-  first_part <- sapply(split_content, function(x) x[1])
-  
-  # Extract the second part (characters after the first blank space)
-  second_part <- sapply(split_content, function(x) ifelse(length(x) > 1, x[2], ""))
-  
-  # Create new columns in the dataframe for the split content
-  identified_otus <- cbind(qseqid = first_part, seqid = second_part, identified_otus[2:ncol(identified_otus)])
-  
-  colnames(identified_otus) <- c("qseqid", "seqid", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
-  
-  cl.max_subset <- cl.max_[, c(1, 3)]
-  
-  cl.max_ordered <- cl.max_subset[match(identified_otus$qseqid, cl.max_subset$qseqid), ]
-  
-  cl.max <- cbind(identified_otus[, 1:9], cl.max_ordered$pident)
-  
-  colnames(cl.max)[ncol(cl.max)] <- "pident"
+# Passo 1: Fazer o left_join mantendo a estrutura de csv1_merged
+csv1_merged <- csv1_merged %>%
+  left_join(
+    identified_otus %>% select(taxid, 2:8),  
+    by = c("tax_ids" = "taxid")             
+  ) %>% relocate(names(identified_otus)[2:8], .after = 3) %>% 
+  select(1:12) 
   
   ########## File = Samples ##########
   
-  Samples <- Samples[order(match(Samples$qseqid, cl.max$qseqid)), ]
+  colnames(Samples)[1] <- "qseqid"
   
-  Final_Match_samples <- plyr::match_df(Samples, cl.max, on = "qseqid")
+  Samples <- Samples %>%
+  inner_join(csv1_merged %>% distinct(qseqid), by = "qseqid")
   
-  Blast <- cbind(cl.max, Final_Match_samples[, 2:length(colnames(Final_Match_samples))])
+  #csv1_merged <- csv1_merged[order(match(csv1_merged$qseqid, Samples$qseqid)), ]
   
-  Blast$Species <- ifelse(Blast$pident >= Genus_Threshold & Blast$pident < Specie_Threshold, "" ,Blast$Species)
-  Blast$Species <- ifelse(Blast$pident >= Family_Threshold & Blast$pident < Genus_Threshold, "" ,Blast$Species)
-  Blast$Genus <- ifelse(Blast$pident >= Family_Threshold & Blast$pident < Genus_Threshold, "" ,Blast$Genus)
+  Samples <- Samples[order(match(Samples$qseqid, csv1_merged$qseqid)), ]
   
-  list_columns <- sapply(Blast, is.list)
-  list_columns_names <- names(list_columns[list_columns])
+  Samples <- Samples %>%
+  left_join(
+    csv1_merged %>% select(qseqid, 2:12),  
+    by = c("qseqid" = "qseqid")             
+  ) %>% 
+  relocate(names(csv1_merged)[2:12], .after = 1)
   
-  Blast[list_columns_names] <- lapply(Blast[list_columns_names], function(col) {
-    sapply(col, function(x) {
-      if (is.null(x)) {
-        return(NA)
-      } else if (is.list(x) || length(x) > 1) {
-        return(as.character(x[[1]]))  
-      } else {
-        return(as.character(x)) 
-      }
-    })
-  })
+  raw_otus <- data.frame(
+  qseqid = names(fasta_seq),
+  Fasta = as.character(fasta_seq),
+  stringsAsFactors = FALSE)
+  
+  Blast <- merge(Samples, raw_otus, by = "qseqid")
   
   Blast <- as.data.frame(Blast, stringsAsFactors = FALSE)
-  
+
   write.table(Blast, file = paste0(Directory, "taxonomic_assignment.txt"), sep = "\t", row.names = F, col.names = T)
-  
-  # Find the Zotus not identified with perc_identity = 97
-  remaining_sequences <- subset(Samples$qseqid, !(Samples$qseqid %in% unique(csv1$qseqid)))
-  
-  if (length(remaining_sequences) != 0) { 
-    # Create a file with a list of Zotus not identified to first threshold of perc_identity
-    write.table(remaining_sequences, file = paste0(Directory, "remaining_sequences.txt"), sep = " ", row.names = F, col.names = F, quote = F)
-    print("End of Run")
-  } else {
-    print("End of Run")
-  }
+    
+  print("End of Run")
 }
--- R/refDB_SubsetNcbiGbif.R
+++ R/refDB_SubsetNcbiGbif.R
@@ -23,146 +23,174 @@
 #' @export
 refDB_SubsetNcbiGbif <- function(gbif_database, cleaned_ncbi_database, ncbi_database_based_on_gbif, genus_flexibility) {
   
+  # Always create an empty output file to start
   file.create(ncbi_database_based_on_gbif, showWarnings = FALSE)
   fileConn <- file(ncbi_database_based_on_gbif, open = "wt")
   close(fileConn)
   
-  if (genus_flexibility == TRUE) {
-    # If genus_flexibility is TRUE:
-    system(paste0("
-      wsl names=$(<", gbif_database ,")
-
-      # Function to split names into chunks
-      split_names_into_chunks() {
-          local chunk_size=$1
-          local num_chunks=$(( (${#names} + $chunk_size - 1) / $chunk_size ))
-          local chunks=()
-          for (( i = 0; i < $num_chunks; i++ )); do
-              local start=$(( $i * $chunk_size ))
-              chunks+=( \"$(echo \"${names:$start:$chunk_size}\")\" )
-          done
-          echo \"${chunks[@]}\"
-      }
-
-      # Function to replace spaces with underscores except the last one
-      replace_spaces_with_underscores() {
-          sed 's/ /_/g'
-      }
-
-      # Function to search and append sequences with exact matching based on the part after the first underscore
-      search_and_append() {
-          local chunk=\"$1\"
-          awk -v pattern=\"$chunk\" '
-              BEGIN {
-                  split(pattern, pat, \"|\")
-                  for (p in pat) {
-                      counts[pat[p]] = 0
-                  }
-              }
-              /^>/ {
-                  header = substr($0, 2)  # Remove the initial >
-                  split(header, parts, \"_\")
-                  if (parts[2] == pat[1] || parts[1] == pat[1]) {  # Match part after or before the first underscore
-                      counts[pat[1]]++
-                      print \">\" counts[pat[1]] \"_\" header
-                      getline
-                      print
-                  }
-              }' ", cleaned_ncbi_database ," | replace_spaces_with_underscores >> ", ncbi_database_based_on_gbif ,"
-      }
-
-      # Split names into chunks of 1000 names each
-      chunk_size=1000
-      IFS='|' read -ra chunks <<< \"$(split_names_into_chunks $chunk_size)\"
-
-      # Search and append sequences for each chunk
-      for chunk in \"${chunks[@]}\"
-      do
-          search_and_append \"$chunk\"
-      done
-    "))
+  # If gbif_database exists and is provided
+  if (!is.null(gbif_database) && file.exists(gbif_database)) {
     
-      # Remove trailing underscores and adjust the headers
-  system(paste0("wsl sed -i '/^>.*_$/s/_$//' ", ncbi_database_based_on_gbif))
-  
-  # Path to the file where you want to save the excluded parts
-  excluded_parts_file <- "SelectedSequences.txt"
-  
-  # Extract and save the excluded parts
-  system(paste0("wsl sed -n 's/^[^_]*_\\([^_]*\\)_.*/\\1/p' ", ncbi_database_based_on_gbif, " | paste -sd '|' - | sed 's/$/|/' > ", excluded_parts_file))
-  #system(paste0("wsl sed -n 's/^>\\([^_]*\\)_.*/\\1/p' ", ncbi_database_based_on_gbif, " | paste -sd '|' - | sed 's/$/|/' > ", excluded_parts_file))
+    if (genus_flexibility == TRUE) {
+      # -------------- (1) GENUS_FLEXIBILITY == TRUE CODE --------------
+      system(paste0("
+        wsl names=$(<", gbif_database ,")
 
-  
-  # Remove the first underscore and everything before the second underscore
-  system(paste0("wsl sed -i 's/_[^_]*_/_/' ", ncbi_database_based_on_gbif))
-  #system(paste0("wsl sed -i 's/^>[^_]*_\\([^_]*_[^_]*\\).*/>\\1/' ", ncbi_database_based_on_gbif))
-    
+        # Function to split names into chunks
+        split_names_into_chunks() {
+            local chunk_size=$1
+            local num_chunks=$(( (${#names} + $chunk_size - 1) / $chunk_size ))
+            local chunks=()
+            for (( i = 0; i < $num_chunks; i++ )); do
+                local start=$(( $i * $chunk_size ))
+                chunks+=( \"$(echo \"${names:$start:$chunk_size}\")\" )
+            done
+            echo \"${chunks[@]}\"
+        }
+
+        # Function to replace spaces with underscores except the last one
+        replace_spaces_with_underscores() {
+            sed 's/ /_/g'
+        }
+
+        # Function to search and append sequences with exact matching
+        # based on the part after the first underscore
+        search_and_append() {
+            local chunk=\"$1\"
+            awk -v pattern=\"$chunk\" '
+                BEGIN {
+                    split(pattern, pat, \"|\")
+                    for (p in pat) {
+                        counts[pat[p]] = 0
+                    }
+                }
+                /^>/ {
+                    header = substr($0, 2)  # Remove >
+                    split(header, parts, \"_\")
+                    if (parts[2] == pat[1] || parts[1] == pat[1]) {
+                        counts[pat[1]]++
+                        print \">\" counts[pat[1]] \"_\" header
+                        getline
+                        print
+                    }
+                }' ", cleaned_ncbi_database ," | replace_spaces_with_underscores >> ", ncbi_database_based_on_gbif ,"
+        }
+
+        # Split names into chunks of 1000
+        chunk_size=1000
+        IFS='|' read -ra chunks <<< \"$(split_names_into_chunks $chunk_size)\"
+
+        # Search and append sequences for each chunk
+        for chunk in \"${chunks[@]}\"
+        do
+            search_and_append \"$chunk\"
+        done
+      "))
+
+      # Final cleanup steps for genus_flexibility == TRUE
+      system(paste0("wsl sed -i '/^>.*_$/s/_$//' ", ncbi_database_based_on_gbif))
+      excluded_parts_file <- "SelectedSequences.txt"
+      system(paste0("wsl sed -n 's/^[^_]*_\\([^_]*\\)_.*/\\1/p' ",
+                    ncbi_database_based_on_gbif, 
+                    " | paste -sd '|' - | sed 's/$/|/' > ", 
+                    excluded_parts_file))
+      system(paste0("wsl sed -i 's/_[^_]*_/_/' ", ncbi_database_based_on_gbif))
+
+    } else {
+      # -------------- (2) GENUS_FLEXIBILITY == FALSE CODE --------------
+      system(paste0("
+        wsl names=$(<", gbif_database ,")
+
+        # Function to split names into chunks
+        split_names_into_chunks() {
+            local chunk_size=$1
+            local num_chunks=$(( (${#names} + $chunk_size - 1) / $chunk_size ))
+            local chunks=()
+            for (( i = 0; i < $num_chunks; i++ )); do
+                local start=$(( $i * $chunk_size ))
+                chunks+=( \"$(echo \"${names:$start:$chunk_size}\")\" )
+            done
+            echo \"${chunks[@]}\"
+        }
+
+        # Function to search and append sequences
+        search_and_append() {
+            local chunk=\"$1\"
+            awk '/^>/{f=0} 
+                 /^>.*'\"$chunk\"'/{f=1} 
+                 f' ", cleaned_ncbi_database ," >> ", ncbi_database_based_on_gbif ,"
+        }
+
+        modified_names=$(echo \"$names\" | sed 's/[[:space:]]/_/g')
+
+        # Split modified names into chunks of 1000
+        chunk_size=1000
+        IFS='|' read -ra chunks <<< \"$(split_names_into_chunks $chunk_size)\"
+
+        # Search and append sequences for each chunk
+        for chunk in \"${chunks[@]}\"
+        do
+            c=0
+            search_and_append \"$chunk\"
+        done
+      "))
+
+      # Final cleanup steps for genus_flexibility == FALSE
+      system(paste0("wsl sed -i '/^>.*_$/s/_$//' ", ncbi_database_based_on_gbif))
+      excluded_parts_file <- "SelectedSequences.txt"
+      system(paste0("wsl sed -n 's/^>\\([^_]*\\)_.*/\\1/p' ",
+                    ncbi_database_based_on_gbif, 
+                    " | paste -sd '|' - | sed 's/$/|/' > ", 
+                    excluded_parts_file))
+      system(paste0("wsl sed -i 's/^>[^_]*_\\([^_]*_[^_]*\\).*/>\\1/' ", ncbi_database_based_on_gbif))
+
+      # Optional final pass to rename duplicates
+      system(paste0("
+        wsl awk '
+          /^>/ {
+            name = substr($0, 2)
+            if (!seen[name]++) {sequence[name]=1}
+            print \">\" sequence[name] \"_\" name
+            sequence[name]++
+          } 
+          !/^>/ {print $0}
+        ' ", ncbi_database_based_on_gbif, " > temp_file && mv temp_file ", ncbi_database_based_on_gbif))
+    }
+
   } else {
-    # if FALSE:
-    system(paste0("
-      wsl names=$(<", gbif_database ,")
+    # -------------- (3) NO gbif_database PROVIDED --------------
+    # Run only your final commands (or any minimal editing you desire)
+    # For example, if you only want to do the cleanup steps on 
+    # the existing 'ncbi_database_based_on_gbif' file without using gbif_database:
 
-      # Function to split names into chunks
-      split_names_into_chunks() {
-          local chunk_size=$1
-          local num_chunks=$(( (${#names} + $chunk_size - 1) / $chunk_size ))
-          local chunks=()
-          for (( i = 0; i < $num_chunks; i++ )); do
-              local start=$(( $i * $chunk_size ))
-              chunks+=( \"$(echo \"${names:$start:$chunk_size}\")\" )
-          done
-          echo \"${chunks[@]}\"
-      }
+    #system(paste0("wsl sed -i '/^>.*_$/s/_$//' ", cleaned_ncbi_database))
+    
+    # Path to the file where you want to save the excluded parts
+    excluded_parts_file <- "SelectedSequences.txt"
 
-  # Function to search and append sequences
-  search_and_append() {
-      local chunk=\"$1\"
-      awk '/^>/{f=0} 
-           /^>.*'\"$chunk\"'/{f=1} 
-           f' ", cleaned_ncbi_database ," >> ", ncbi_database_based_on_gbif ,"
+    # Extract and save the excluded parts
+    system(paste0("wsl sed -n 's/^>\\([^_]*\\)_.*/\\1/p' ",
+              cleaned_ncbi_database, 
+              " | paste -sd '|' - | sed 's/$/|/' > ", 
+              excluded_parts_file))
+    
+    #
+    #system(paste0(
+    #"wsl sed 's/^>\\([^_]*\\)_.*/>\\1/' ",
+    #cleaned_ncbi_database, 
+    #" > ",
+    #ncbi_database_based_on_gbif
+    #))
+
+    system(paste0(
+    "wsl sed 's/^>\\(.*[0-9]\\)[^_]*_.*$/>\\1/' ",
+    cleaned_ncbi_database,
+    " > ",
+    ncbi_database_based_on_gbif
+    ))
+    
+
+    #Remove the first underscore and everything before the second underscore
+    #system(paste0("wsl sed -i 's/_[^_]*_/_/' ", cleaned_ncbi_database))
   }
-
-      # Replace spaces with underscores in names
-      modified_names=$(echo \"$names\" | sed 's/[[:space:]]/_/g')
-
-      # Split modified names into chunks of 1000 names each
-      chunk_size=1000
-      IFS='|' read -ra chunks <<< \"$(split_names_into_chunks $chunk_size)\"
-
-      # Search and append sequences for each chunk
-      for chunk in \"${chunks[@]}\"
-      do
-          c=0 # Reset counter for each chunk
-          search_and_append \"$chunk\"
-      done
-    "))
-   
-            # Remove trailing underscores and adjust the headers
-  system(paste0("wsl sed -i '/^>.*_$/s/_$//' ", ncbi_database_based_on_gbif))
-  
-  # Path to the file where you want to save the excluded parts
-  excluded_parts_file <- "SelectedSequences.txt"
-  
-  # Extract and save the excluded parts
-  #system(paste0("wsl sed -n 's/^[^_]*_\\([^_]*\\)_.*/\\1/p' ", ncbi_database_based_on_gbif, " | paste -sd '|' - | sed 's/$/|/' > ", excluded_parts_file))
-  system(paste0("wsl sed -n 's/^>\\([^_]*\\)_.*/\\1/p' ", ncbi_database_based_on_gbif, " | paste -sd '|' - | sed 's/$/|/' > ", excluded_parts_file))
-
-  
-  # Remove the first underscore and everything before the second underscore
-  #system(paste0("wsl sed -i 's/_[^_]*_/_/' ", ncbi_database_based_on_gbif))
-  system(paste0("wsl sed -i 's/^>[^_]*_\\([^_]*_[^_]*\\).*/>\\1/' ", ncbi_database_based_on_gbif))
-
-  system(paste0("
-  wsl awk '
-    /^>/ {
-      name = substr($0, 2)                     
-      if (!seen[name]++) {sequence[name]=1}    
-      print \">\" sequence[name] \"_\" name    
-      sequence[name]++                         
-    } 
-    !/^>/ {print $0}                           
-  ' ", ncbi_database_based_on_gbif, " > temp_file && mv temp_file ", ncbi_database_based_on_gbif))
-     
-  }
-  
 }
