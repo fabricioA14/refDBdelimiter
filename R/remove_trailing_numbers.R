407848e5fb4bfdd1aaaa878eeaf592675fcf05ff
fabricioA14 <fabriciodosanjos14l@gmail.com>
2025-09-01 11:22:36 -0300

    Updated remove_trailing_numbers function

--- R/remove_trailing_numbers.R
+++ R/remove_trailing_numbers.R
@@ -1,14 +1,19 @@
-#' Remove Trailing Numbers from Names
+#' Remove Trailing Coordinates from Names
 #'
-#' This function removes trailing numbers from names.
+#' This function removes trailing coordinates of the form `_lon_lat` 
+#' (numbers separated by underscores) from taxon names.
 #'
-#' @param name A character vector of names from which trailing numbers should be removed.
-#' @return A character vector with trailing numbers removed.
+#' @param name A character vector of names from which trailing coordinates should be removed.
+#' @return A character vector with trailing coordinates removed.
 #' @examples
-#' names <- c("Homo sapiens 1", "Pan troglodytes 2", "Gorilla gorilla 3")
+#' names <- c("Pyrenula nitida_18.515806_54.396611",
+#'            "Evernia prunastri_-12.009611_54.537694",
+#'            "Scoliciosporum umbrinum_18.455_54.303611")
 #' remove_trailing_numbers(names)
+#' # [1] "Pyrenula nitida"        "Evernia prunastri"      
+#' # [3] "Scoliciosporum umbrinum"
 #' @import readr
 #' @export
 remove_trailing_numbers <- function(name) {
-  gsub(" [0-9]+$", "", name)
+  sub("_[0-9\\.-]+_[0-9\\.-]+$", "", name)
 }
